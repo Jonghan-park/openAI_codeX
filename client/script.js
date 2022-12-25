e4debbde4ed819d6c2a71c55b254dde0141117a0
Jonghan Park <jonghan.park@edu.sait.ca>
2022-12-25 14:21:55 -0700

    Update front functions

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import bot from "./assets/bot.svg";
-import user from "./assets/user.avg";
+import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
@@ -16,3 +16,70 @@ function loader(element) {
     }
   }, 300);
 }
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index)
+      index++
+    }else {
+      clearInterval(interval)
+    }
+  }, 20);
+}
+
+function generateUniqueId(){
+  const timestamp = Date.now()
+  const randomNumber = Math.random()
+  const hexadecimalString = randomNumber.toString(16)
+
+  return `id-${timestamp}-${hexadecimalString}`
+}
+
+function chatStripe (isAi, value, uniqueId){
+  return (
+    `
+    <div class='wrapper ${isAi && 'ai'}'>
+      <div class='chat'>
+        <div className="profile">
+          <img 
+            src="${isAi ? bot : user}"
+            alt="${isAi ? 'bot' : 'user'}"
+          />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+    `
+  )
+}
+ 
+const handleSumbit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user's chatStripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  form.reset()
+
+  //bot's chatStripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId) 
+
+  chatContainer.scrollTop = chatContainer.scrollHeight
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv)
+}
+
+form.addEventListener('submit', handleSumbit)
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13){
+    handleSumbit(e)
+  }
+})
